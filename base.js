ca7a85863022c7817d8957b4942014afc456e8db
Chirag Kaushik <kaushikchirag187@gmail.com>
2026-01-03 22:07:05 +0530

    project prd done

---
+++ base.js
@@ -0,0 +1,21 @@
+/*
+At start of the project we do: npm init --> ye ek node ecosystem set up krdeta h so that package.json file bnti h
+which is identity card + config file of the project
+
+why need?: Because Node projects depend on:
+             external libraries (React, Express, Mongoose, etc.)
+             scripts (start, build, test)
+             versions (very important in real-world projects)
+
+             package.json answers questions like:
+            📦 What is this project?
+            🔢 Which dependencies does it use?
+            ⚙️ How do I run it?
+            🧠 Which version is stable? etcc
+aapse details lega and json file bnadega
+
+what is npm init -y then? --> zyada details ni mngta fas fas json file bnadeta h
+
+ab jse manle tune express install kia ya kuch aisa install kia toh vo khud json me change hojaegaa bina kch add kre thats so important
+now the thing is package json me type:"modules" krle to have some requirement like this 
+ */
